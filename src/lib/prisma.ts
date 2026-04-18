eb2e0a23eebf42ac0f12a3f6e1960f1073167c92
MarcusDevHub <marcusthuliok9@gmail.com>
2026-04-18 14:23:42 -0300

    fix: na verdade precisa de adapter...

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,14 @@
 import 'dotenv/config';
 import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+
 
 // Aqui definimos a URL do banco SQLite.
 // Se DATABASE_URL existir, usamos ela.
 // Se não existir, usamos o arquivo local dev.db.
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL!,
+})
 
 // Truque para evitar múltiplas instâncias do Prisma em desenvolvimento
 const globalForPrisma = globalThis as unknown as {
@@ -12,13 +17,13 @@ const globalForPrisma = globalThis as unknown as {
 
 // Se já existir uma instância global, reutiliza.
 // Se não existir, cria uma nova com o adapter do SQLite.
-export const prisma =
-    globalForPrisma.prisma ?? new PrismaClient({ log: ['error', 'warn'] })
 
-// Em desenvolvimento, guarda a instância no objeto global
-// para evitar recriar o Prisma a cada hot reload do Next.js.
+export const prisma =
+    globalForPrisma.prisma ??
+    new PrismaClient({ adapter, log: ['error', 'warn'] })
+
 if (process.env.NODE_ENV !== 'production') {
-    globalForPrisma.prisma = prisma;
+    globalForPrisma.prisma = prisma
 }
 
 export default prisma;
